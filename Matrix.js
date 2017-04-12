0c63416bfdba49fbd2a4b4082cd2e79bf3689654
Etilawin <kimik812@gmail.com>
2017-04-12 22:21:17 +0200

    Multiplication

--- Matrix.js
+++ Matrix.js
@@ -30,4 +30,39 @@ function Matrix(a,b){
       }
     }
   }
+
+  this.sub = function(other){
+    if(other instanceof Matrix){
+      if(other.rows === this.rows && other.cols === this.cols){
+        var r = this.rows;
+        var c = this.cols;
+        for(var i = 0; i < r; i++){
+          for(var j = 0; j < c; j++){
+            this.matrix[i][j] -= other.matrix[i][j];
+          }
+        }
+      } else {
+        console.log("Matrix isn't of the same dimension")
+      }
+    } else {
+      console.log("Couldn't add a non-matrix");
+    }
+  }
+
+  this.mult = function(other){
+    if(other instanceof Matrix){
+      if(this.cols === other.rows){
+        var newMatrix = newMatrix(this.rows, other.cols);
+        var line;
+        for(var i = 0; i < this.rows; i++){
+          line = 0;
+          for(var j = 0; j < other.cols; j++){
+            for(var g = 0; g < other.rows; g++){
+              line += other.matrix[g][j]*this.matrix[i][] // TODO
+            }
+          }
+        }
+      }
+    }
+  }
 }
